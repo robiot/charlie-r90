1b87afbc6230e712942348edea1a13b618f975fc
Elliot <me@robiot.dev>
2022-11-27 23:15:14 +0200

    Create move_servo function

--- src/main.rs
+++ src/main.rs
@@ -1,63 +1,54 @@
 #![no_std]
 #![no_main]
 
-use arduino_hal::{
+// gyro sensor, Adafruit LSM6DSOX 6 DoF
 
-    Peripherals, Pins
+use core::ops::Range;
+
+use arduino_hal::{
+    hal::port::PB1,
+    port::{mode::Output, Pin},
 };
 use panic_halt as _;
 
-struct ChairlieFlightComputer {
-    dp: Peripherals,
-    pins: Pins,
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+
+fn servo_pwm(x: i32, pin: &mut Pin<Output, PB1>) {
+    let val = (((x * 1025) / 100) + 500) as u32;
+
+    pin.set_high();
+    arduino_hal::delay_us(val);
+    pin.set_low();
+
+    arduino_hal::delay_ms(10);
 }
 
-impl ChairlieFlightComputer {
-    fn setup(self) {
-        let mut serial = arduino_hal::default_serial!(self.dp, self.pins, 57600);
-    
-        ufmt::uwriteln!(&mut serial, "Booting up...").unwrap();
-        ufmt::uwriteln!(&mut serial, "Charlie R90 Flight Computer Alpha-1.0.0").unwrap();    
-    }
-
-    // fn service_loop(self) {
-
-    // }
-
-    fn init(self) {
-        let mut serial = arduino_hal::default_serial!(self.dp, self.pins, 57600);
-    
-        ufmt::uwriteln!(&mut serial, "Booting up...").unwrap();
-
-        // self.setup();
-
-        // loop {
-        //     self.service_loop();
-        // }
-    }
-
-    /**
-     * Create new struct
-     */
-    pub fn new() -> Self {
-        let pins = arduino_hal::pins!(arduino_hal::Peripherals::take().unwrap());
-        ChairlieFlightComputer { dp: arduino_hal::Peripherals::take().unwrap(), pins }
+// Example:
+// from: 180
+// to: 90
+fn move_servo(pin: &mut Pin<Output, PB1>, from: i32, to: i32) {
+    if from > to {
+        for i in (to..from).rev() {
+            servo_pwm(i, pin)
+        }
+    } else {
+        for i in from..to {
+            servo_pwm(i, pin)
+        }
     }
 }
 
 #[arduino_hal::entry]
 fn main() -> ! {
-    ChairlieFlightComputer::new().init();
-    loop {}
-    // let dp = arduino_hal::Peripherals::take().unwrap();
-    // let pins = arduino_hal::pins!(dp);
+    let dp = arduino_hal::Peripherals::take().unwrap();
+    let pins = arduino_hal::pins!(dp);
 
-    // let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
+    let mut serial = arduino_hal::default_serial!(dp, pins, 57600);
 
-    // let mut pwm_pin = pins.d9.into_output();
+    let mut pwm_pin = pins.d9.into_output();
 
-    // ufmt::uwriteln!(&mut serial, "Booting up...").unwrap();
-    // ufmt::uwriteln!(&mut serial, "Charlie R90 Flight Computer Alpha-1.0.0").unwrap();
+    ufmt::uwriteln!(&mut serial, "Booting up...").unwrap();
+    ufmt::uwriteln!(&mut serial, "Charlie R90 Flight Computer Alpha-{}", VERSION).unwrap();
 
     // ufmt::uwriteln!(&mut serial, "Initiating launch sequence").unwrap();
     // ufmt::uwriteln!(&mut serial, "3").unwrap();
@@ -69,32 +60,34 @@ fn main() -> ! {
     // ufmt::uwriteln!(&mut serial, "Ignition").unwrap();
     // arduino_hal::delay_ms(500);
 
-    // loop {
-        // led.set_high();
-        // arduino_hal::delay_us(500);
-        // led.set_low();
-        // arduino_hal::delay_ms(20);
-    // }
+    loop {
+        // servo_pwm(90, &mut pwm_pin);
+        // ufmt::uwriteln!(&mut serial, "val: {}", 0).unwrap();
+        // for i in 0..90 {
+        //     servo_pwm(i, &mut pwm_pin);
 
-    // Important because this sets the bit in the DDR register!
+        //     let val = (((i * 1025) / 100) + 500) as u32;
+        //     ufmt::uwriteln!(&mut serial, "val: {}", val).unwrap();
+        // }
 
-    // - TC1 runs off a 250kHz clock, with 5000 counts per overflow => 50 Hz signal.
-    // - Each count increases the duty-cycle by 4us.
-    // - Use OC1A which is connected to D9 of the Arduino Uno.
-    // let tc1 = dp.TC1;
-    // tc1.icr1.write(|öw| unsafe { w.bits(4999) });
+        move_servo(&mut pwm_pin, 180, 90);
 
-    // tc1.tccr1a
-    //     .write(|w| w.wgm1().bits(2).com1a().match_clear());
-    // tc1.tccr1b
-    //     .write(|w| w.wgm1().bits(3).cs1().prescale_64());
+        arduino_hal::delay_ms(20);
 
-    // loop {
-    // 100 counts => 0.4ms
-    // 700 counts => 2.8ms
-    // for duty in 100..=700 {
-    //     tc1.ocr1a.write(|w| unsafe { w.bits(duty) });
-    //     arduino_hal::delay_ms(20);
-    // }
-    // }
+        move_servo(&mut pwm_pin, 90, 180);
+
+
+        arduino_hal::delay_ms(1000);
+
+        // move_servo_to(&mut pwm_pin, 160..180, true);
+
+        // for i in (0..90).rev() {
+        //     servo_pwm(i, &mut pwm_pin);
+
+        //     let val = (((i * 1025) / 100) + 500) as u32;
+        //     ufmt::uwriteln!(&mut serial, "vala: {}", val).unwrap();
+        // }
+
+        arduino_hal::delay_ms(1000);
+    }
 }
